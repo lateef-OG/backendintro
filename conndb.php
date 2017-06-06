016281c7b585ab460ba0dd34f66ae8c411312d9d
lateef <lateef>
2017-06-06 21:07:49 +0100

    DB connection

---
+++ conndb.php
@@ -0,0 +1,17 @@
+<?php
+	echo "Hello World";
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "myDB";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	} 
+	echo "connected to Database successfully!";
+?>	
